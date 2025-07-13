cde01bc7e45e31513e8928bb8869f5a551117e27
nethmi-1997 <63659987+nethmi-1997@users.noreply.github.com>
2025-07-13 10:55:21 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_yRxX0buyg', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1074vhvfskgvuve9o8hkm3qeil', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
